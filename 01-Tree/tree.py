b898f77e2841b94a291f2c49b5630b87018dbbba
Stein-jpg <pflanzerare@gmail.com>
2025-06-24 12:45:26 +0200

    add chars and print them

--- 01-Tree/tree.py
+++ 01-Tree/tree.py
@@ -1,1 +1,10 @@
-print("hello world")
+def print_directory(path):
+    # Unicode-Zeichen
+    TEE = chr(9500)  # ├
+    ELBOW = chr(9492)  # └
+    VERT = chr(9474)  # │
+    DASH = chr(9472)  # ─
+    print(TEE, ELBOW, VERT, DASH)
+
+
+print_directory(".")
